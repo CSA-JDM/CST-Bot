0c1b3679344594a08aeafc15c10e98c2952fe39a
Jacob Meadows <2178@chaparralstaracademy.com>
2017-12-01 16:28:58 -0600

    Edited indentions
    just changed indentions (12/1/17)

--- CST Bot.py
+++ CST Bot.py
@@ -28,49 +28,55 @@ async def on_ready():
 
 @client.event
 async def on_message(msg):
-	if msg.author.id == "118845112599052288" and msg.content.startswith("!calc"):
-		result = eval(msg.content[6:])
-		await client.send_message(msg.channel, str(result))
-	elif msg.content.startswith('!roll'):
-		print(time.asctime() + ": " + msg.author.name + ": " + msg.content)
-		print(roll(msg) + "\n")
-		await client.send_message(msg.channel, roll(msg))
-	elif msg.content.startswith("!uptime"):
-		print(time.asctime() + ": " + msg.author.name + ": " + msg.content)
-		uptime = time.time() - start
-		upday = int(uptime//day)
-		uphour = int((uptime % day) // hour)
-		upminute = int((uptime % hour) // minute)
-		upsecond = int((uptime % minute) // 1)
-		print("Live for " + ("{}:{}:{}:{} (d:h:m:s)".format(upday, uphour, upminute, upsecond) if uptime > day else ("{}:{}:{} (h:m:s)".format(uphour, upminute, upsecond) if uptime>hour else ("{}:{} (m:s)".format(upminute, upsecond) if uptime>minute else "{} seconds".format(upsecond)))) + "\n")
-		await client.send_message(msg.channel, "live for " + ("{}:{}:{}:{} (d:h:m:s)".format(upday, uphour, upminute, upsecond) if uptime > day else ("{}:{}:{} (h:m:s)".format(uphour, upminute, upsecond) if uptime>hour else ("{}:{} (m:s)".format(upminute, upsecond) if uptime>minute else "{} seconds".format(upsecond)))))
-	elif msg.content.startswith("!calc"):                                                            ##### (ALL) CALC
-		expression = msg.content[6:].lower()
-		print(str(msg.content))
-		for x in expression:
-			if str(x) not in "0123456789abcdefintx+-=<>~^*/%&|()[].,\"' \r\n\x00":
-				await client.send_message(msg.channel, "Characters allowed are 0123456789abcdefintx+-=<>~^*/%&|()[].,\"' and spaces")
-				return
-		try:
-			result=eval(expression)
-			print(result)
-			await client.send_message(msg.channel, str(result))
-		except:
-			await client.send_message(msg.channel, "Well shoot")
-	elif msg.content.startswith('!fibonacci'):
-		print(time.asctime() + ": " + msg.author.name + ": " + msg.content)
-		if "100." in msg.content: 
-			a, b = 0, 1
-			while b < 100:
-				await client.send_message(msg.channel, b)
-				print(b)
-				a, b = b, a + b
-		if "1000." in msg.content: 
-			a, b = 0, 1
-			while b < 1000:
-				await client.send_message(msg.channel, b)
-				print(b)
-				a, b = b, a + b
+    if msg.author.id == "118845112599052288" and msg.content.startswith("!calc"):
+        result = eval(msg.content[6:])
+        await client.send_message(msg.channel, str(result))
+    elif msg.content.startswith('!roll'):
+        print(time.asctime() + ": " + msg.author.name + ": " + msg.content)
+        print(roll(msg) + "\n")
+        await client.send_message(msg.channel, roll(msg))
+    elif msg.content.startswith("!uptime"):
+        print(time.asctime() + ": " + msg.author.name + ": " + msg.content)
+        uptime = time.time() - start
+        upday = int(uptime // day)
+        uphour = int((uptime % day) // hour)
+        upminute = int((uptime % hour) // minute)
+        upsecond = int((uptime % minute) // 1)
+        print("Live for " + ("{}:{}:{}:{} (d:h:m:s)".format(upday, uphour, upminute, upsecond) if uptime > day else (
+        "{}:{}:{} (h:m:s)".format(uphour, upminute, upsecond) if uptime > hour else (
+        "{}:{} (m:s)".format(upminute, upsecond) if uptime > minute else "{} seconds".format(upsecond)))) + "\n")
+        await client.send_message(msg.channel, "live for " + (
+        "{}:{}:{}:{} (d:h:m:s)".format(upday, uphour, upminute, upsecond) if uptime > day else (
+        "{}:{}:{} (h:m:s)".format(uphour, upminute, upsecond) if uptime > hour else (
+        "{}:{} (m:s)".format(upminute, upsecond) if uptime > minute else "{} seconds".format(upsecond)))))
+    elif msg.content.startswith("!calc"):  ##### (ALL) CALC
+        expression = msg.content[6:].lower()
+        print(str(msg.content))
+        for x in expression:
+            if str(x) not in "0123456789abcdefintx+-=<>~^*/%&|()[].,\"' \r\n\x00":
+                await client.send_message(msg.channel,
+                                          "Characters allowed are 0123456789abcdefintx+-=<>~^*/%&|()[].,\"' and spaces")
+                return
+        try:
+            result = eval(expression)
+            print(result)
+            await client.send_message(msg.channel, str(result))
+        except:
+            await client.send_message(msg.channel, "Well shoot")
+    elif msg.content.startswith('!fibonacci'):
+        print(time.asctime() + ": " + msg.author.name + ": " + msg.content)
+        if "100." in msg.content:
+            a, b = 0, 1
+            while b < 100:
+                await client.send_message(msg.channel, b)
+                print(b)
+                a, b = b, a + b
+        if "1000." in msg.content:
+            a, b = 0, 1
+            while b < 1000:
+                await client.send_message(msg.channel, b)
+                print(b)
+                a, b = b, a + b
 
 
 def roll(msg):
@@ -79,10 +85,13 @@ def roll(msg):
         try:
             splitted = msg.content.lower()[6:].split('d')
             if splitted[0] != '' and type(int(splitted[0])) is int and type(int(splitted[1])) is int:
-                roll = [randint(1,int(splitted[1])) for x in range(int(splitted[0]))]
-                return msg.author.name + ", rolling " + splitted[1] + " sided dice " + splitted[0] + (" time: " if int(splitted[0]) == 1 else " times: ") + "\n" + str(sum(roll)) + " (" + '+'.join([str(x) for x in roll]) + ")"
+                roll = [randint(1, int(splitted[1])) for x in range(int(splitted[0]))]
+                return msg.author.name + ", rolling " + splitted[1] + " sided dice " + splitted[0] + (
+                " time: " if int(splitted[0]) == 1 else " times: ") + "\n" + str(sum(roll)) + " (" + '+'.join(
+                    [str(x) for x in roll]) + ")"
             elif splitted[0] == '' and type(int(splitted[1])) is int:
-                return msg.author.name + ", rolling " + splitted[1] + " sided dice 1 time: " + str(randint(1, int(splitted[1])))
+                return msg.author.name + ", rolling " + splitted[1] + " sided dice 1 time: " + str(
+                    randint(1, int(splitted[1])))
             else:
                 return "Format incorrect"
         except:
